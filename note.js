7cf74ddb56e05019c8cfb2c8a0ed014f9f98095d
rudravasan <142309837+rudravasan@users.noreply.github.com>
2023-10-19 00:30:33 +0530

    Update note.js

--- note.js
+++ note.js
@@ -1,47 +1,25 @@
-const notesList = document.getElementById('notes-list');
 const noteInput = document.getElementById('note-input');
-const addNoteButton = document.getElementById('add-note');
-const editNoteButton = document.getElementById('edit-note');
-const deleteNoteButton = document.getElementById('delete-note');
-let selectedNoteIndex = -1;
+const saveButton = document.getElementById('save-button');
 
-addNoteButton.addEventListener('click', addNote);
-editNoteButton.addEventListener('click', editNote);
-deleteNoteButton.addEventListener('click', deleteNote);
-notesList.addEventListener('click', selectNote);
+saveButton.addEventListener('click', saveNote);
 
-function addNote() {
+function saveNote() {
     const noteText = noteInput.value.trim();
     if (noteText !== '') {
-        const noteItem = document.createElement('li');
-        noteItem.textContent = noteText;
-        notesList.appendChild(noteItem);
-        noteInput.value = '';
+        // Create a Blob containing the text
+        const blob = new Blob([noteText], { type: 'text/plain' });
+
+        // Create a URL for the Blob
+        const url = URL.createObjectURL(blob);
+
+        // Create a download link
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'note.txt';
+        a.textContent = 'Download Note';
+
+        // Append the link to the page
+        document.body.appendChild(a);
     }
 }
 
-function editNote() {
-    if (selectedNoteIndex !== -1) {
-        const noteText = noteInput.value.trim();
-        if (noteText !== '') {
-            notesList.children[selectedNoteIndex].textContent = noteText;
-            noteInput.value = '';
-        }
-    }
-}
-
-function deleteNote() {
-    if (selectedNoteIndex !== -1) {
-        notesList.removeChild(notesList.children[selectedNoteIndex]);
-        noteInput.value = '';
-        selectedNoteIndex = -1;
-    }
-}
-
-function selectNote(event) {
-    const target = event.target;
-    if (target.nodeName === 'LI') {
-        noteInput.value = target.textContent;
-        selectedNoteIndex = Array.from(notesList.children).indexOf(target);
-    }
-}
